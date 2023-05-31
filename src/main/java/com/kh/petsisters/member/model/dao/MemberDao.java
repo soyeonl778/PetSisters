ecc1697c903e717c7d1bd193555f4f71ad633d0c
Jeong-JaeHoon1 <ne4355@gmail.com>
2023-05-31 15:59:27 +0900

    member 오류 수정

--- src/main/java/com/kh/petsisters/member/model/dao/MemberDao.java
+++ src/main/java/com/kh/petsisters/member/model/dao/MemberDao.java
@@ -1,9 +1,11 @@
 package com.kh.petsisters.member.model.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.kh.petsisters.member.model.vo.Member;
 
+@Repository
 public class MemberDao {
 	
 	public Member login(SqlSessionTemplate sqlSession, Member m) {
--- src/main/java/com/kh/petsisters/member/model/service/MemberServiceImpl.java
+++ src/main/java/com/kh/petsisters/member/model/service/MemberServiceImpl.java
@@ -2,10 +2,12 @@ package com.kh.petsisters.member.model.service;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.kh.petsisters.member.model.dao.MemberDao;
 import com.kh.petsisters.member.model.vo.Member;
 
+@Service
 public class MemberServiceImpl implements MemberService{
 	
 	@Autowired
