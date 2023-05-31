f3a219ca18f087531be1ef5172ce3ebb36b2633d
Jeong-JaeHoon1 <ne4355@gmail.com>
2023-05-31 16:57:52 +0900

    petsitter 기능 오류 수정

---
+++ src/main/java/com/kh/petsisters/petsitter/model/service/PetSitterServiceImpl.java
@@ -0,0 +1,24 @@
+package com.kh.petsisters.petsitter.model.service;
+
+import org.springframework.stereotype.Service;
+
+import com.kh.petsisters.petsitter.model.vo.PetSitter;
+
+@Service
+public class PetSitterServiceImpl implements  PetSitterService {
+
+	@Override
+	public PetSitter selectPetSitter(int pNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updatePetSitter(PetSitter p) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
