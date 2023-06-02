7f4e8995a5fa4f5f5a5275f5f1ee9f4e08f9f9cf
Juheun Lee <juheunee716@gmail.com>
2023-06-02 18:21:08 +0900

    인터셉터 공지사항용 등록

---
+++ src/main/java/com/kh/petsisters/common/interceptor/AdminInterceptor.java
@@ -0,0 +1,32 @@
+package com.kh.petsisters.common.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.kh.petsisters.member.model.vo.Member;
+
+public class AdminInterceptor extends HandlerInterceptorAdapter {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request
+						   , HttpServletResponse response
+						   , Object handler) throws Exception {
+		HttpSession session = request.getSession();
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		if(loginUser.getUserName().equals("관리자")) {
+			return true;
+		} else {
+			session.setAttribute("alertMsg", "관리자만 접근 가능한 페이지입니다.");
+			response.sendRedirect(request.getContextPath() + "/");
+			
+			return false;
+		}
+	
+	}
+						 
+
+}
