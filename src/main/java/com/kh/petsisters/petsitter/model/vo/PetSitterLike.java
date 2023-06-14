88214302b5d324ba7f242c02ca8052abd68e9387
daeunnkkk <kekdms91@gmail.com>
2023-06-14 16:02:56 +0900

    펫시터 찜 VO 추가

---
+++ src/main/java/com/kh/petsisters/petsitter/model/vo/PetSitterLike.java
@@ -0,0 +1,18 @@
+package com.kh.petsisters.petsitter.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class PetSitterLike {
+
+	private int userNo;          //	USER_NO	NUMBER
+	private int refPno;          //	REF_PNO	NUMBER
+}
